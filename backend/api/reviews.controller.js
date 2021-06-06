096d066cfdb7089f024381eade761554ce65e301
VasuNVerma <vermvasun@gmail.com>
2021-06-06 20:24:13 +0530

    "Added Reviews APIs"

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -2,10 +2,10 @@ import ReviewsDAO from "../dao/reviewsDAO.js"
 
 export default class ReviewsController {
 
-    static async apiPostReview(req, res, next){
-        try{
-            const restaurantId = req.query.restaurant_id
-            const review = req.query.text
+    static async apiPostReview(req, res, next) {
+        try {
+            const restaurantId = req.body.restaurant_id
+            const review = req.body.text
             const userInfo = {
                 name: req.body.name,
                 _id: req.body.user_id
@@ -20,9 +20,58 @@ export default class ReviewsController {
                 date
             )
 
-            res.json({status: "success"})
-        } catch(e){
-            res.status(500).json({error: e.message})
+            res.json({ status: "success" })
+        } catch (e) {
+            res.status(500).json({ error: e.message })
+        }
+    }
+
+    static async apiUpdateReview(req, res, next) {
+
+        try {
+            const reviewId = req.body.review_id
+            const text = req.body.text
+            const userId = req.body.user_id
+
+            const date = new Date()
+
+            const ReviewResponse = await ReviewsDAO.updateReview(
+                reviewId,
+                userId,
+                text,
+                date
+            )
+
+            var { error } = ReviewResponse
+            if (error) {
+                res.status(400).json({ error })
+            }
+
+            if (ReviewResponse.modifiedCount === 0) {
+                throw new Error(
+                    "unable to update review - user may not be original poster"
+                )
+            }
+
+            res.json({ status: "success" })
+        } catch (e) {
+            res.status(500).json({ error: e.message })
+        }
+    }
+
+    static async apiDeleteReview(req, res, next) {
+        try {
+            const reviewId = req.query.review_id
+            const userId = req.body.user_id
+
+            const ReviewResponse = await ReviewsDAO.deleteReview(
+                reviewId,
+                userId,
+            )
+
+            res.json({ status: "success" })
+        } catch (e) {
+            res.status(500).json({ error: e.message })
         }
     }
 }
